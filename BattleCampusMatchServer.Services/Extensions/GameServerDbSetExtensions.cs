5b4f0372a6e65b14562ccb92debd159d15a08924
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-01-13 16:01:42 +0900

    Added helper extension for finding GameServerModel by IpportInfo

---
+++ BattleCampusMatchServer.Services/Extensions/GameServerDbSetExtensions.cs
@@ -0,0 +1,23 @@
+﻿using BattleCampus.Core;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BattleCampusMatchServer.Services.Extensions
+{
+    public static class GameServerDbSetExtensions
+    {
+        public static async Task<GameServerModel> TryFindGameServerModel(this DbSet<GameServerModel> servers, IpPortInfo ipPortInfo)
+        {
+            var server = await servers.FirstOrDefaultAsync(x =>
+                x.IpPortInfo.IpAddress == ipPortInfo.IpAddress
+                && x.IpPortInfo.DesktopPort == ipPortInfo.DesktopPort
+                && x.IpPortInfo.WebsocketPort == ipPortInfo.WebsocketPort);
+
+            return server;
+        }
+    }
+}
