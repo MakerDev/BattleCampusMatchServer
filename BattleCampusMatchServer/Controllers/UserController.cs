c78e748aaec397b416f0654ccdd2bf80c508069f
yujinshin <raki2001@o365.yonsei.ac.kr>
2021-01-14 19:06:26 +0900

    Moved all apis to MatchServer

---
+++ BattleCampusMatchServer/Controllers/UserController.cs
@@ -0,0 +1,31 @@
+﻿using BattleCampus.MatchServer.Application.User;
+using MediatR;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BattleCampusMatchServer.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IMediator _mediator;
+
+        public UserController(IMediator mediator)
+        {
+            _mediator = mediator;
+        }
+
+        [HttpPost("login")]
+        public async Task<ActionResult<AdminUser>> LoginAsAdminAsync([FromBody] Login.Query query)
+        {
+            var adminUser = await _mediator.Send(query);
+
+            return Ok(adminUser);
+        }
+    }
+}
